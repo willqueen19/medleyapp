5d15d0135a601b5ca3bde7466ed310654bfbd200
Will Queen <willqueen@Wills-MacBook-Air.local>
2019-02-06 15:56:06 -0500

    router functional

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-//import { Router, Route, Switch } from 'react-router';
+import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+import Landing from './components/generic/LandingPage'
+import Survey from './components/generic/SurveyPage'
+import Recommend from './components/generic/RecommendationPage';
+import Order from './components/generic/OrderPage'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
-          <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/rec/">About</Link>
-                </li>
-                <li>
-                  <Link to="/users/">Users</Link>
-                </li>
-              </ul>
-            </nav>
+          <Router>
+            <div>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/">Landing</Link>
+                  </li>
+                  <li>
+                    <Link to="/survey/">Survey</Link>
+                  </li>
+                  <li>
+                    <Link to="/users/">Recommend</Link>
+                  </li>
+                  <li>
+                    <Link to="/order/">Order</Link>
+                  </li>
+                </ul>
+              </nav>
 
-            <Route path="/" exact component={Landing} />
-            <Route path="/users/" component={Survey} />
-            <Route path="/rec/" component={RecommendationScreen} />
+              <Route path="/" exact component={ Landing } />
+              <Route path="/survey/" component={ Survey } />
+              <Route path="/rec/" component={ Recommend }/>
+              <Route path="/order/" component={ Order } />
 
-          </div>
-        </Router>
+            </div>
+          </Router>
       </div>
     );
   }
---
+++ src/components/generic/OrderPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+class Order extends Component {
+  render () {
+    return (
+        <div></div>
+    )
+  }
+}
+
+export default Order;
