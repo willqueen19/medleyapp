bd5be4847ea3853fea13759404bc0c76000252f2
Will Queen <willqueen@Wills-MacBook-Air.local>
2019-02-13 16:58:02 -0500

    current

---
+++ src/components/reducers/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from "redux/es/redux";
+import { routerReducer, routerMiddleware, ConnectedRouter } from "react-router-redux";
+
+const allReducers = combineReducers({
+
+});
+
+export default allReducers;
