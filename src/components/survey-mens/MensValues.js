cf9e964e7e8031e0ab2f8e3837180335724cd076
Jaclyn <iulianetti.j@husky.neu.edu>
2019-02-13 16:49:55 -0500

    mens images

--- src/components/survey-mens/MensValues.js
+++ src/components/survey-mens/MensValues.js
@@ -4,9 +4,9 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Button, CardDeck } from 'reactstrap';
 import SurveyCard from '../generic/ThreeSurveyCard.js';
-import img1 from '../../assets/MW-1.jpeg';
-import img2 from '../../assets/MW-2.jpeg';
-import img3 from '../../assets/MW-2.jpeg';
+import img1 from '../../assets/MVal-1.jpeg';
+import img2 from '../../assets/MVal-2.jpeg';
+import img3 from '../../assets/MVal-3.jpeg';
 
 class MensValues extends Component {
 
