152fc13120e005f9fbd967308823e9a7dd5be128
Will Queen <willqueen@Wills-MacBook-Air.local>
2019-04-08 13:13:53 -0400

    not good

--- src/components/generic/RecommendationCard.js
+++ src/components/generic/RecommendationCard.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 //import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Container, Row, Col } from 'reactstrap';
 import { Card, Button, CardImg, CardTitle, CardText,
-  CardSubtitle, CardBody } from 'reactstrap';
+    CardSubtitle, CardBody } from 'reactstrap';
 import { Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import {bindActionCreators} from "redux";
 import * as surveyActions from "../../actions/surveyActions";
@@ -18,14 +17,12 @@ class RecommendationCard extends Component {
         super(props);
         this.state = {
             items: [],
-            item: null,
-            cardState: true
+            item: null
 
         };
 
         this.getNewItem = this.getNewItem.bind(this);
         this.getRandomInt = this.getRandomInt.bind(this);
-        this.changeCardState = this.changeCardState.bind(this);
     }
 
     componentWillMount() {
@@ -63,60 +60,46 @@ class RecommendationCard extends Component {
         this.props.actions.setCurrentOutfit(item, this.props.itemKey);
     };
 
-    changeCardState() {
-        this.setState({
-          cardState: !this.state.cardState
-        })
-    }
 
-  render () {
+    render () {
 
-      // TODO add number of items in array to card, so user has idea of the selection
-      var keyValue = this.props.keyValue;
-      var numItems = this.state.items.length;
-      var item = this.state.item;
-      var itemImage = item.images[0].url;
-      var itemName = item.name;
-      var itemPrice = item.price.formattedValue;
-      var itemSizesObject = item.variantSizes;
-      var itemSizes = [];
-      var itemShow = this.state.cardState;
+        // TODO add number of items in array to card, so user has idea of the selection
 
-      var i;
-      for (i = 0; i < itemSizesObject.length; i++) {
-          itemSizes.push(<option>{itemSizesObject[i].filterCode}</option>)
-      }
+        var keyValue = this.props.keyValue;
+        var item = this.state.item;
+        var itemImage = item.images[0].url;
+        var itemName = item.name;
+        var itemPrice = item.price.formattedValue;
 
-      var card = {
+        var itemSizesObject = item.variantSizes;
+        var itemSizes = [];
+        var itemShow = this.state.cardState;
 
-      }
+        var i;
+        for (i = 0; i < itemSizesObject.length; i++) {
+            itemSizes.push(<option>{itemSizesObject[i].filterCode}</option>)
+        }
 
-      return (
-          <Col className="recCards">
-              <Form>
-                  <FormGroup>
-                      <Label for="exampleSelect">Choose a size</Label>
-                      <Input type="select" name="select" id="exampleSelect">
-                          {itemSizes}
-                      </Input>
-                  </FormGroup>
-              </Form>
-              <Button className="newItem" onClick={this.getNewItem}>New</Button>
-              <Button className="showItem" onClick={this.changeCardState}>Hide/Show</Button>
-              {itemShow ?
-                <Card key={keyValue} className="cardRec">
+        return (
+            <div className="recommendationCard">
+                <Card key={keyValue} className="cardRec" onClick={this.getNewItem}>
                     <CardImg top width="100%" src={itemImage} alt="Card image cap" />
                     <CardBody>
                         <CardTitle>{itemName}</CardTitle>
                         <CardText>{itemPrice}</CardText>
                     </CardBody>
+                    <Form>
+                        <FormGroup>
+                            <Label for="exampleSelect">Choose a size</Label>
+                            <Input type="select" name="select" id="exampleSelect">
+                                {itemSizes}
+                            </Input>
+                        </FormGroup>
+                    </Form>
                 </Card>
-                :
-                <div>hello</div>
-              }
-          </Col>
-    )
-  }
+            </div>
+        )
+    }
 }
 
 function mapStateToProps(state, ownProps) {
