47d0d83414d0e684be0e7566c224dc209921625d
Will Queen <willqueen@Wills-MacBook-Air.local>
2019-04-23 17:17:16 -0400

    final

--- src/components/generic/LandingPage.js
+++ src/components/generic/LandingPage.js
@@ -7,27 +7,25 @@ import logo from '../../assets/Final Logo.png';
 
 
 class Landing extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
 
-  constructor(props) {
-    super(props);
-    this.state = {
+        };
+    }
 
-    };
-  }
-
-  render() {
-
-    return(
-      <div className="landing">
-          <img className="ml-auto" src={logo} alt="Logo" />
-          <p className="lead">
-              <Link to={'/survey/'}>
-                 <Button size="lg" className="start">Build an Outfit</Button>
-              </Link>
-          </p>
-      </div>
-)
-  }
+    render() {
+        return(
+            <div className="landing">
+                <img className="ml-auto" src={logo} alt="Logo" />
+                <p className="lead">
+                    <Link to={'/survey/'}>
+                        <Button className="bigButton" size="lg" className="start">Build an Outfit</Button>
+                    </Link>
+                </p>
+            </div>
+        )
+    }
 }
 
 export default Landing;
